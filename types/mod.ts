a2b0817dab54a211090299f9b098d616b6bf8187
Halvard Mørstad <halvard@simplyundoable.com>
2024-10-16 18:42:08 +0200

    feat(types): added type and improved documentation

--- types/mod.ts
+++ types/mod.ts
@@ -8,11 +8,23 @@ export type FlipMap<T extends Record<keyof T, keyof any>> = {
 
 /**
  * Make properties K in T optional
+ *
+ * @example
+ * ```ts
+ * type A = { a: string, b: string }
+ * type B = PartialBy<A, "b"> // { a: string, b?: string }
+ * ```
  */
 export type PartialBy<T, K extends keyof T> = Omit<T, K> & Partial<Pick<T, K>>;
 
 /**
  * Make properties K in T required
+ *
+ * @example
+ * ```ts
+ * type A = { a?: string, b?: string }
+ * type B = RequiredBy<A, "b"> // { a?: string, b: string }
+ * ```
  */
 export type RequiredBy<T, K extends keyof T> =
   & Omit<T, K>
@@ -20,6 +32,12 @@ export type RequiredBy<T, K extends keyof T> =
 
 /**
  * Make properties K in T required, and the rest partial
+ *
+ * @example
+ * ```ts
+ * type A = { a: string, b?: string, c?: string }
+ * type B = RequiredPartialBy<A, "b"> // { a?: string, b: string, c?: string }
+ * ```
  */
 export type RequiredPartialBy<T, K extends keyof T> =
   & RequiredBy<
@@ -48,6 +66,40 @@ export type WriteableBy<T, K extends keyof T> =
   & Writeable<Pick<T, K>>;
 
 /**
- * Gets the values of an object
+ * Gets the values of a Record
+ *
+ * @example With type
+ * ```ts
+ * type A = { a: "hello", b: "world" }
+ * type B = ValueOf<A> // "hello"|"world"
+ * ```
+ *
+ * @example With object
+ * ```ts
+ * const a = { a: "hello", b: "world" } as const
+ * type B = ValueOf<typeof a> // "hello"|"world"
+ * ```
  */
 export type ValueOf<T> = T[keyof T];
+
+/**
+ * Represents a generic constructor
+ *
+ * @example As argument
+ * ```ts
+ * import type { AnyConstructor } from "@stdext/typings";
+ *
+ * function(SomeClass: AnyConstructor){
+ *  const c = new SomeClass()
+ * }
+ * ```
+ *
+ * @example For other type
+ * ```ts
+ * import type { AnyConstructor } from "@stdext/typings";
+ *
+ * type SomeConstructor = AnyConstructor<SomeClass, [string, SomeOptions]>
+ * ```
+ */
+// deno-lint-ignore no-explicit-any
+export type AnyConstructor<T, A extends any[] = any[]> = new (...args: A) => T;
